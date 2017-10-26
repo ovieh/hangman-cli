c1d49d77ad3d58311eea1914c6674d252e231467
Ovieh <ovieh.mosley@gmail.com>
2017-10-26 10:00:41 -0400

    Created letter array and underscore array prototypes.

--- letter.js
+++ letter.js
@@ -1,5 +1,29 @@
 const Word = require('./word.js')
 
 class Letter {
-    
+    constructor() {
+        const state = new Word();
+        this.state = state;
+    }
+    printWord() {
+        console.log(this.state.word)
+    }
+
 }
+
+Letter.prototype.parseArray = function(){
+    let letterArray = this.state.word.split('');
+    return letterArray;
+}
+
+Letter.prototype.parseUnderscore = function() {
+    let underScoreArray = Array.from(this.parseArray());    
+    console.log(underScoreArray);
+    return underScoreArray.fill("_");
+}
+
+let blah = new Letter;
+// blah.printWord();
+console.log(blah.parseUnderscore());
+
+module.exports = Letter;
--- word.js
+++ word.js
@@ -1,17 +1,13 @@
 const chance = require('chance').Chance();
 
-
 const wordArray = [];
 let randomWord;
 
 for (let i = 0; i < 10; i++) {
-    wordArray.push(chance.state({
-        full: true
-    }));
+    wordArray.push(chance.state( { full: true } ));
 }
 randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
 
-// wordArray.forEach( word => { console.log(word) });
 
 class Word {
     constructor(word=randomWord) {
@@ -19,9 +15,9 @@ class Word {
     }
 
 }
-const word = new Word();
+// const word = new Word();
 
-console.log(word.word);
+// console.log(word.word);
 
 
 module.exports = Word;
